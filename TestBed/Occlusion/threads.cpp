923f3ba859257576e322ad420b98f8762a2a5d84
Oles <oles@localhost>
2002-09-11 13:42:54 +0000

    Temporary file created by Visual Studio .NET to detect Microsoft Visual SourceSafe capabilities.

--- TestBed/Occlusion/threads.cpp
+++ TestBed/Occlusion/threads.cpp
@@ -1,14 +1,11 @@
 #include "stdafx.h"
+#include "ftimer.h"
+#include "process.h"
 
 LPVOID	fiber_sheduler		= 0;
 LPVOID	fiber_processor		= 0;
 
 volatile int	dummy		= 0;
-
-VOID	CALLBACK		terminator	(UINT uID, UINT uMsg, DWORD dwUser, DWORD dw1, DWORD dw2)
-{
-	SwitchToFiber		(fiber_sheduler);
-}
 
 VOID	CALLBACK		t_process	(PVOID P)
 {
@@ -22,13 +19,42 @@ VOID	CALLBACK		t_process	(PVOID P)
 
 void	t_sheduler		()
 {
-	timeSetEvent		(10,0,terminator,0,TIME_ONESHOT | TIME_CALLBACK_FUNCTION);
 	SwitchToFiber		(fiber_processor);
 }
 
-void	t_test()
+volatile u64 test		= 0;
+void __cdecl t_thread	(void * P)
 {
-	// 
+	for (;;) test++;
+}
+
+#define TESTS	1000
+
+void	t_test			()
+{
+	CTimer				T;
+	SetPriorityClass	(GetCurrentProcess(),HIGH_PRIORITY_CLASS);
+
+	// Make sure the thread is started
+	_beginthread		(t_thread,0,0);
+	Sleep				(5);
+
+	u64		C			= 0;
+	u64		iter		= 0;
+	for (int i=0; i<TESTS; i++)
+	{
+		T.Start				();
+		test				=	0;
+		Sleep				(0);
+		iter				+=	test;
+		C					+=	T.GetElapsed	();
+	}
+	SetPriorityClass	(GetCurrentProcess(),NORMAL_PRIORITY_CLASS);
+	float mcs			=  float(u64(C)/u64(TESTS))*CPU::cycles2milisec;
+	iter				/= u64(TESTS);
+	printf				("%f, it: %d\n",mcs,int(iter));
+
+	// Create fibers
 	fiber_sheduler		= ConvertThreadToFiber	(0);
 	fiber_processor		= CreateFiber			(0,t_process,0);
 
