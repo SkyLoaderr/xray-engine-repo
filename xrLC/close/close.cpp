8f5e4ec5a190b931f5d79bbe09b91bb3fb53b737
Oles <oles@localhost>
2002-11-06 10:54:08 +0000

    Temporary file created by Visual Studio .NET to detect Microsoft Visual SourceSafe capabilities.

---
+++ xrLC/close/close.cpp
@@ -0,0 +1,18 @@
+// close.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "close.h"
+
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+
+	return 0;
+}
+
+
+
