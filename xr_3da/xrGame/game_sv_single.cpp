4faac4effd0ec583f944c83016aef7686a9758e9
Oles <oles@localhost>
2002-12-06 15:06:45 +0000

    Temporary file created by Visual Studio .NET to detect Microsoft Visual SourceSafe capabilities.

--- xr_3da/xrGame/game_sv_single.cpp
+++ xr_3da/xrGame/game_sv_single.cpp
@@ -8,7 +8,40 @@ void	game_sv_Single::Create			(LPCSTR options)
 
 BOOL	game_sv_Single::OnTouch			(u16 eid_who, u16 eid_what)
 {
-	return TRUE;
+	xrServer*			S		= Level().Server;
+	xrServerEntity*		e_who	= S->ID_to_entity(eid_who);		VERIFY(e_who	);
+	xrServerEntity*		e_what	= S->ID_to_entity(eid_what);	VERIFY(e_what	);
+
+	xrSE_Actor*			A		= dynamic_cast<xrSE_Actor*> (e_who);
+	if (A)
+	{
+		// Actor touches something
+		xrSE_Weapon*	W			=	dynamic_cast<xrSE_Weapon*> (e_what);
+		if (W) 
+		{
+			// Weapon
+			vector<u16>&	C			=	A->children;
+			u8 slot						=	W->get_slot	();
+			for (u32 it=0; it<C.size(); it++)
+			{
+				xrServerEntity*		Et	= S->ID_to_entity				(C[it]);
+				if (0==Et)				continue;
+				xrSE_Weapon*		T	= dynamic_cast<xrSE_Weapon*>	(Et);
+				if (0==T)				continue;
+				if (slot == T->get_slot())	
+				{
+					// We've found same slot occupied - disallow ownership
+					return FALSE;
+				}
+			}
+
+			// Weapon slot empty - ownership OK
+			return TRUE;
+		}
+	}
+
+	// We don't know what the hell is it, so disallow ownership just for safety 
+	return FALSE;
 }
 
 BOOL	game_sv_Single::OnDetouch		(u16 eid_who, u16 eid_what)
