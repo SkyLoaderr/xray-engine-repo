c42d43c778790943e7309813cf545a1e0a20ace3
Manowar <manowar@localhost>
2004-04-28 10:19:05 +0000

    Sony DADC Copy Protection

--- xr_3da/CopyProtection.h
+++ xr_3da/CopyProtection.h
@@ -1,8 +1,41 @@
 #pragma once
 
 #ifdef	USE_COPYPROTECTION
-IC	void	CheckCopyProtection	()	{
-	// manowar has to do something here :)
+
+#pragma pack(push,1)
+typedef struct SECUROM_PID{
+	int pid_error;
+	unsigned long pid_version;
+	union{
+		unsigned char pid_buf[264];
+		struct{
+			unsigned char pid_reserved[3];
+			unsigned char pid_type;
+			unsigned long pid_length;
+			unsigned char pid_data[256];
+		};
+	};
+}tSECUROM_PID;
+#pragma pack(pop)
+
+#include <windows.h>
+
+IC	void	CheckCopyProtection	()
+{
+	DWORD ret;
+
+	__asm{
+		mov eax,8791h;
+		mov ebx,2107h;
+		mov ecx,6345h;
+	};
+	ret=GetVersion();
+
+	if(ret!=0x6345){
+		MessageBox(NULL,"Copy protection violation!","Error",MB_OK | MB_ICONHAND);
+		TerminateProcess(GetCurrentProcess(),0x66);
+	}
+
 }
 #else
 IC	void	CheckCopyProtection	()	{
